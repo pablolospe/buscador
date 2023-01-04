9f5b2f21ae809279bf17b0fe49ebcff26fe2de67
pablolospe <yosoypxl@gmail.com>
2023-01-03 22:42:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+const people = [
+  {
+    id: 'people-01',
+    title: 'Juan Perez'
+  },
+  {
+    id: 'people-02',
+    title: 'Juana Sanchez'
+  },
+  {
+    id: 'people-03',
+    title: 'Francisco Vignia'
+  },
+  {
+    id: 'people-04',
+    title: 'Pablo Lopez'
+  },
+  {
+    id: 'people-05',
+    title: 'Ramon Ortega'
+  },
+]
+
+const calendar = [
+  {
+    id: 'calendar-01',
+    title: 'Sesión de seguimiento'
+  },
+  {
+    id: 'calendar-02',
+    title: 'Revisión de propuestas'
+  },
+  {
+    id: 'calendar-03',
+    title: 'Evento para donar juguetes'
+  },
+  {
+    id: 'calendar-04',
+    title: 'Junta semanal de equipo'
+  },
+  {
+    id: 'calendar-05',
+    title: 'Revisión de pendientes con cliente'
+  },
+]
+
+const emails = [
+  {
+    id: 'email-01',
+    title: 'Reporte de resultados'
+  },
+  {
+    id: 'email-02',
+    title: 'Requisitos para el cambio'
+  },
+  {
+    id: 'email-03',
+    title: 'Estatus de funcionalidad'
+  },
+  {
+    id: 'email-04',
+    title: 'Proximos eventos'
+  },
+  {
+    id: 'email-05',
+    title: 'Participa en la encuesta'
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hola mundus
     </div>
   );
 }
